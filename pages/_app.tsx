317f12569ca0a1cc1de1d4bb26df9f15edd782d4
Siris <vincentcmv@gmail.com>
2022-06-03 18:26:01 +0530

    feat: initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Title</title>
+				<meta property='og:title' content='Title' />
+				<meta property='og:description' content='description' />
+				<meta property='theme-color' content='#000000' />
+				<meta name='theme-color' content='#000000' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta name='description' content='description' />
+			</Head>
+			<Component {...pageProps} />{' '}
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
